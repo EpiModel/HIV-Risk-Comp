9d5a4c049fe141ecabacc868b27dbb381a00d060
smjenness <sjenness@uw.edu>
2016-11-17 12:08:50 -0500

    Add installation file

---
+++ scripts/Installation.R
@@ -0,0 +1,6 @@
+
+## Install stack to run scripts
+
+devtools::install_github("statnet/EpiModel")
+devtools::install_github("statnet/tergmLite", subdir = "tergmLite", ref = "preFastEl")
+devtools::install_github("statnet/EpiModelHIV", ref = "hiv-risk-comp")
